e3ea176c771069777889755943bfa9a00211eb29
w3bdev <w3bdev@protonmail.com>
2022-02-11 23:17:19 +0530

    Get directory items

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use std::env;
+use std::{env, fs, path::PathBuf};
 
 fn main() {
     let path = env::current_dir().unwrap();
-    println!("{:?}", path)
+    let items = get_dir_items(&path);
+    println!("{:?}", items)
+}
+
+fn get_dir_items(path: &PathBuf) -> Vec<PathBuf> {
+    fs::read_dir(path)
+        .unwrap()
+        .map(|item| item.unwrap())
+        .map(|item| item.path())
+        .collect()
 }
