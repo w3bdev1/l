bc5818223b803f27b3ccdd2fec6d54953e3fccf5
w3bdev <w3bdev@protonmail.com>
2022-02-11 23:22:47 +0530

    List directory items

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use std::{env, fs, path::PathBuf};
 fn main() {
     let path = env::current_dir().unwrap();
     let items = get_dir_items(&path);
-    println!("{:?}", items)
+
+    for item in items {
+        println!("• { }", item.file_name().unwrap().to_str().unwrap())
+    }
 }
 
 fn get_dir_items(path: &PathBuf) -> Vec<PathBuf> {
