79312252541d49603e68646fcb5682897da91669
w3bdev <w3bdev@protonmail.com>
2022-02-15 18:01:35 +0530

    File size added

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use std::path::PathBuf;
+use std::{os::unix::prelude::MetadataExt, path::PathBuf};
 
 // Colors
 const RESET_COLOR: &str = "\x1b[0m";
-const BLUE_BOLD: &str = "\x1b[94;1m";
+const DIR_COLOR: &str = "\x1b[94;1m";
+const SIZE_COLOR: &str = "\x1b[95m";
 
 #[derive(Debug)]
 pub struct Files {
@@ -19,10 +20,12 @@ impl Files {
         for item in path.read_dir().expect("Could not read the directory") {
             if let Ok(item) = item {
                 let item_path = item.path();
+                let item_metadata = item.metadata().unwrap();
                 let name = String::from(item_path.file_name().unwrap().to_str().unwrap());
                 let is_dir = item_path.is_dir();
+                let size = human_readable_size(item_metadata.size());
 
-                files.push(File { name, is_dir })
+                files.push(File { name, is_dir, size })
             }
         }
 
@@ -31,14 +34,7 @@ impl Files {
 
     pub fn render(self) {
         for f in self.files {
-            match f.is_dir {
-                true => {
-                    println!("• {}{}{}", &BLUE_BOLD, f.name, RESET_COLOR)
-                }
-                false => {
-                    println!("• {}", f.name)
-                }
-            }
+            println!("{}", print_file(&f, f.is_dir))
         }
     }
 }
@@ -47,4 +43,39 @@ impl Files {
 struct File {
     name: String,
     is_dir: bool,
+    size: String,
+}
+
+fn print_file(f: &File, is_dir: bool) -> String {
+    if is_dir {
+        format!(
+            "• {size_color}{size} {dir_color}{dir_name}{reset_color}",
+            size_color = SIZE_COLOR,
+            size = f.size,
+            dir_color = DIR_COLOR,
+            dir_name = f.name,
+            reset_color = RESET_COLOR
+        )
+    } else {
+        format!(
+            "• {size_color}{size}{reset_color} {file_name}",
+            size_color = SIZE_COLOR,
+            size = f.size,
+            reset_color = RESET_COLOR,
+            file_name = f.name
+        )
+    }
+}
+
+fn human_readable_size(size: u64) -> String {
+    let mut size = size;
+    let units = ['B', 'K', 'M', 'G', 'T', 'P', 'E'];
+    let mut unit_index = 0;
+
+    while size > 1000 {
+        size = size / 1000;
+        unit_index += 1;
+    }
+
+    format!("{:4}{}", size, units[unit_index])
 }
